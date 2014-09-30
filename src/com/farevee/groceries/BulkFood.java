0016bc645a6ad5938f0a1779e6f1794844d423a8
Yazan Kittaneh <kittaneh17@grinnell.edu>
2014-09-30 12:29:46 -0500

    part c

--- src/com/farevee/groceries/BulkFood.java
+++ src/com/farevee/groceries/BulkFood.java
@@ -3,4 +3,30 @@ package com.farevee.groceries;
 public class BulkFood
 {
 
+  // +--------+------------------------------------------------------
+  // | Fields |
+  // +--------+
+  String bulkName;
+  Units bulkUnit;
+  int bulkPrice;
+  int bulkSupply;
+  
+  
+  // +--------------+------------------------------------------------
+  // | Constructors |
+  // +--------------+
+  public BulkFood(String name, Units unit, int pricePerUnit, int supply)
+  {
+    bulkName=name;
+    bulkUnit=unit;
+    bulkPrice=pricePerUnit;
+    bulkSupply=supply;
+  }
+  
+  // +---------+----------------------------------------------------
+  // | Methods |
+  // +---------+
+  
+  
+ 
 }
--- src/com/farevee/groceries/Weight.java
+++ src/com/farevee/groceries/Weight.java
@@ -2,5 +2,14 @@ package com.farevee.groceries;
 
 public class Weight
 {
+  
+  Units unit;
+  int amount;
+  
+  public Weight(Units un, int value)
+  {
+    unit = un;
+    amount = value;
+  }
 
 }
